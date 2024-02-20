956e446efde6c8451d67607a52b7b7dc8fad2cae
AR4AN_-_KHALIFA <129208285+AR4AN-MUAZ@users.noreply.github.com>
2024-02-20 20:11:54 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -1,1 +1,7 @@
-import myfile
+#!/data/data/com.termux/files/usr/bin/python
+import platform,os,sys
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import myfile
+elif bit == '32bit':
+    print(" [#] Sorry bit is not frequent right now")
